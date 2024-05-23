265083fcca9966d836dfab332b37c33144759498
Daniel Carvalho <daniel.m.carvalho@tecnico.ulisboa.pt>
2024-05-23 16:41:31 +0100

    feat(assignment): implemented type checking for assignment node

--- targets/type_checker.cpp
+++ targets/type_checker.cpp
@@ -228,23 +228,17 @@ void til::type_checker::do_rvalue_node(cdk::rvalue_node *const node, int lvl) {
 
 void til::type_checker::do_assignment_node(cdk::assignment_node *const node, int lvl) {
   ASSERT_UNSPEC;
-
-  try {
-    node->lvalue()->accept(this, lvl);
-  } catch (const std::string &id) {
-    auto symbol = std::make_shared<til::symbol>(cdk::primitive_type::create(4, cdk::TYPE_INT), id, 0);
-    _symtab.insert(id, symbol);
-    _parent->set_new_symbol(symbol);  // advise parent that a symbol has been inserted
-    node->lvalue()->accept(this, lvl);  //DAVID: bah!
-  }
-
-  if (!node->lvalue()->is_typed(cdk::TYPE_INT)) throw std::string("wrong type in left argument of assignment expression");
-
+  node->lvalue()->accept(this, lvl + 2);
   node->rvalue()->accept(this, lvl + 2);
-  if (!node->rvalue()->is_typed(cdk::TYPE_INT)) throw std::string("wrong type in right argument of assignment expression");
-
-  // in Simple, expressions are always int
-  node->type(cdk::primitive_type::create(4, cdk::TYPE_INT));
+  if (node->rvalue()->is_typed(cdk::TYPE_UNSPEC)) {
+    node->rvalue()->type(node->lvalue()->type());
+  } 
+  
+  if (node->lvalue()->type() == node->rvalue()->type()) {
+    node->type(node->lvalue()->type());
+  } else {
+    throw std::string("wrong types in assignment");
+  }
 }
 
 //---------------------------------------------------------------------------
