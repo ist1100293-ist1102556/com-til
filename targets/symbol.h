337840f2a5ef9da331b38bec9d14095288e38e62
Carlos Felgueiras <carlosfelgueiras@tecnico.ulisboa.pt>
2024-05-24 23:11:35 +0000

    feat(semantic-analysis): implemented type checking of declarations

--- targets/symbol.h
+++ targets/symbol.h
@@ -10,11 +10,15 @@ namespace til {
   class symbol {
     std::shared_ptr<cdk::basic_type> _type;
     std::string _name;
+    ssize_t _offset = 0; // Since a real offset cannot be 0, if a symbol 
+                     // has offset 0, it means that it is a global variable.
+    bool _is_main = false;
+    int _qualifier = 0;
     long _value; // hack!
 
   public:
-    symbol(std::shared_ptr<cdk::basic_type> type, const std::string &name, long value) :
-        _type(type), _name(name), _value(value) {
+    symbol(std::shared_ptr<cdk::basic_type> type, const std::string &name, int qualifier) :
+        _type(type), _name(name), _qualifier(qualifier) {
     }
 
     virtual ~symbol() {
@@ -29,6 +33,24 @@ namespace til {
     }
     const std::string &name() const {
       return _name;
+    }
+    ssize_t offset() {
+      return _offset;
+    }
+    void offset(ssize_t offset) {
+      _offset = offset;
+    }
+    bool is_main() {
+      return _is_main;
+    }
+    void is_main(bool is_main) {
+      _is_main = is_main;
+    }
+    int qualifier() {
+      return _qualifier;
+    }
+    void qualifier(int qualifier) {
+      _qualifier = qualifier;
     }
     long value() const {
       return _value;
