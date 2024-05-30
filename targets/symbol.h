99d93a2d045346a09e2161710ee46b7d86721416
Carlos Felgueiras <carlosfelgueiras@tecnico.ulisboa.pt>
2024-05-30 14:57:11 +0000

    refactor(codegen): removed unused variable in symbol

--- targets/symbol.h
+++ targets/symbol.h
@@ -12,7 +12,6 @@ namespace til {
     std::string _name;
     ssize_t _offset = 0; // Since a real offset cannot be 0, if a symbol 
                      // has offset 0, it means that it is a global variable.
-    bool _is_main = false;
     int _qualifier = 0;
 
   public:
@@ -38,12 +37,6 @@ namespace til {
     }
     void offset(ssize_t offset) {
       _offset = offset;
-    }
-    bool is_main() {
-      return _is_main;
-    }
-    void is_main(bool is_main) {
-      _is_main = is_main;
     }
     int qualifier() {
       return _qualifier;
